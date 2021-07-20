dc7ded0614d78b8a4107e9d9fe520d905eb0f2ba
itinerare <adestus@gmail.com>
2021-07-20 10:20:45 -0400

    remove tiny spoiler plugin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -139,10 +139,10 @@
                     convert_urls: false,
                     plugins: [
                         'advlist autolink lists link image charmap print preview anchor',
-                        'searchreplace visualblocks code fullscreen spoiler',
+                        'searchreplace visualblocks code fullscreen',
                         'insertdatetime media table paste code help wordcount'
                     ],
-                    toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | spoiler-add spoiler-remove | removeformat | code',
+                    toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | removeformat | code',
                     content_css: [
                         '{{ asset('css/app.css') }}',
                         '{{ asset('css/itinerare.css') }}'
