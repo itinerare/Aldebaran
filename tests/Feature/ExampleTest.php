ddc3508864e1e964779820d8346753d22ab16e88
itinerare <ad@itinerare.net>
2022-01-30 18:51:23 -0500

    feat(tests): set up and ensure very basic test functioning

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,17 +2,23 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function testBasicTest()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
+    /**
+     * Test getting the main page.
+     */
+    public function test_CanGetIndex()
+    {
+        // The main page's contents are dependent on text pages existing,
+        // so make sure they're present.
+        $this->artisan('add-text-pages');
+
+        $response = $this->get('/');
         $response->assertStatus(200);
     }
 }
