476bee0ced3d31b05c1e732fc2021b0a43e1bee0
itinerare <ad@itinerare.net>
2022-03-23 12:47:19 -0400

    feat(layout): add full-width layout option

--- config/aldebaran/settings.php
+++ config/aldebaran/settings.php
@@ -24,12 +24,20 @@ return [
         'projects_nav' => 0,
     ],
 
-    // Whether galleries should display images in columns or rows.
+    'layout' => [
+        // Whether or not the site should be full-width
+        // By default content is confined to the center portion
+        // This is overridden for the admin panel/
+        // anywhere there is a navigation sidebar
+        'full_width' => 0,
+    ],
+
+    // Whether galleries should display images in columns or rows
     // Should be either 'rows' or 'columns'
     'gallery_arrangement' => 'rows',
 
     // Image dimensions, in px.
-    // Which thumbnail dimension is used depends on the setting above.
+    // Which thumbnail dimension is used depends on the setting above
     'thumbnail_width'    => 250,
     'thumbnail_height'   => 200,
     'display_image_size' => 2000,
