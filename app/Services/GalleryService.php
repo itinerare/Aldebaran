4e222efdfd8c7f0334cfe0d2d27e808a50fe527d
itinerare <ad@itinerare.net>
2023-01-13 17:08:18 -0500

    fix(pieces): image extension not properly set per config on image creation

--- app/Services/GalleryService.php
+++ app/Services/GalleryService.php
@@ -337,15 +337,16 @@ class GalleryService extends Service {
 
             // Record data for the image
             $image = PieceImage::create([
-                'piece_id'         => $piece->id,
-                'hash'             => randomString(15),
-                'fullsize_hash'    => randomString(15),
-                'extension'        => $data['image']->getClientOriginalExtension(),
-                'description'      => $data['description'] ?? null,
-                'alt_text'         => $data['alt_text'],
-                'is_primary_image' => $data['is_primary_image'] ?? 0,
-                'is_visible'       => $data['is_visible'] ?? 0,
-                'data'             => $data['data'],
+                'piece_id'          => $piece->id,
+                'hash'              => randomString(15),
+                'fullsize_hash'     => randomString(15),
+                'extension'         => config('aldebaran.settings.image_formats.full') ?? $data['image']->getClientOriginalExtension(),
+                'description'       => $data['description'] ?? null,
+                'alt_text'          => $data['alt_text'],
+                'is_primary_image'  => $data['is_primary_image'] ?? 0,
+                'is_visible'        => $data['is_visible'] ?? 0,
+                'data'              => $data['data'],
+                'display_extension' => config('aldebaran.settings.image_formats.display') && config('aldebaran.settings.image_formats.display') != config('aldebaran.settings.image_formats.full') ? config('aldebaran.settings.image_formats.display') : null,
             ]);
 
             $this->processImage($data, $image);
