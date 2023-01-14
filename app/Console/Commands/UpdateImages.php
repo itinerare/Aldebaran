1737466fc3c39cd9546de44bebeeb2e790ae4ab8
itinerare <ad@itinerare.net>
2023-01-14 12:02:27 -0500

    feat(commands): add option to update-images to just update DB

--- app/Console/Commands/UpdateImages.php
+++ app/Console/Commands/UpdateImages.php
@@ -57,96 +57,40 @@ class UpdateImages extends Command {
             }
 
             $this->info("\n".'The next section concerns piece images. Please see config/aldebaran/settings.php for the relevant settings.');
+            $this->info('While this command can convert piece images for you, if you have a large number of images, and/or a number of large images, it\'s recommended to batch-convert them through other means due to this process potentially being resource-intensive and consequently having adverse impacts on your site.');
+            $this->info('If you wish to do so, you can choose to instead adjust pieces\' information in the database without altering the images.');
 
-            if (config('aldebaran.settings.image_formats.full') && (config('aldebaran.settings.image_formats.display') == config('aldebaran.settings.image_formats.full') || config('aldebaran.settings.image_formats.display') == null) && $this->confirm('Do you want to update all piece images to '.config('aldebaran.settings.image_formats.full').' now?'.(config('aldebaran.settings.image_formats.admin_view') ? ' Note that they will appear as '.config('aldebaran.settings.image_formats.admin_view').' files in the admin panel for convenience.' : ''))) {
-                $this->line('Updating all piece images...');
+            if ($this->confirm('Would you like to do so now? Otherwise, you will be given the option to fully process images.')) {
+                $this->line('Updating piece image records...');
                 $bar = $this->output->createProgressBar(PieceImage::all()->count());
                 $bar->start();
-                foreach (PieceImage::all() as $image) {
-                    if (file_exists($image->imagePath.'/'.$image->fullsizeFileName)) {
-                        $fullFile = Image::make($image->imagePath.'/'.$image->fullsizeFileName);
-                        if ($fullFile->mime() != config('aldebaran.settings.image_formats.full')) {
-                            unlink($image->imagePath.'/'.$image->fullsizeFileName);
-                        } else {
-                            unset($fullFile);
-                        }
-                    }
-                    if (file_exists($image->imagePath.'/'.$image->imageFileName)) {
-                        $displayFile = Image::make($image->imagePath.'/'.$image->imageFileName);
-                        if ($displayFile->mime() != config('aldebaran.settings.image_formats.full')) {
-                            unlink($image->imagePath.'/'.$image->imageFileName);
-                        } else {
-                            unset($displayFile);
-                        }
-                    }
-                    if (file_exists($image->imagePath.'/'.$image->thumbnailFileName)) {
-                        $thumbFile = Image::make($image->imagePath.'/'.$image->thumbnailFileName);
-                        if ($thumbFile->mime() != config('aldebaran.settings.image_formats.full')) {
-                            unlink($image->imagePath.'/'.$image->thumbnailFileName);
-                        } else {
-                            unset($thumbFile);
-                        }
-                    }
 
+                foreach (PieceImage::all() as $image) {
                     $image->update([
                         'extension'         => config('aldebaran.settings.image_formats.full'),
-                        'display_extension' => null,
+                        'display_extension' => config('aldebaran.settings.image_formats.display') && config('aldebaran.settings.image_formats.display') != config('aldebaran.settings.image_formats.full') ? config('aldebaran.settings.image_formats.display') : null,
                     ]);
-
-                    if (isset($fullFile)) {
-                        $fullFile->save($image->imagePath.'/'.$image->fullsizeFileName, null, config('aldebaran.settings.image_formats.full'));
-                        unset($fullFile);
-                    }
-                    if (isset($displayFile)) {
-                        $displayFile->save($image->imagePath.'/'.$image->imageFileName, null, config('aldebaran.settings.image_formats.full'));
-                        unset($displayFile);
-                    }
-                    if (isset($thumbFile)) {
-                        $thumbFile->save($image->imagePath.'/'.$image->thumbnailFileName, null, config('aldebaran.settings.image_formats.full'));
-                        unset($thumbFile);
-                    }
-
                     $bar->advance();
                 }
+
                 $bar->finish();
-                $this->line("\n");
             } else {
-                if (config('aldebaran.settings.image_formats.full') && $this->confirm('Do you want to update piece full-size images to '.config('aldebaran.settings.image_formats.full').' now?'.(config('aldebaran.settings.image_formats.admin_view') ? ' Note that they will appear as '.config('aldebaran.settings.image_formats.admin_view').' files in the admin panel for convenience.' : ''))) {
-                    $this->line('Updating piece full-size images...');
+                if (config('aldebaran.settings.image_formats.full') && (config('aldebaran.settings.image_formats.display') == config('aldebaran.settings.image_formats.full') || config('aldebaran.settings.image_formats.display') == null) && $this->confirm('Do you want to update all piece images to '.config('aldebaran.settings.image_formats.full').' now?'.(config('aldebaran.settings.image_formats.admin_view') ? ' Note that they will appear as '.config('aldebaran.settings.image_formats.admin_view').' files in the admin panel for convenience.' : ''))) {
+                    $this->line('Updating all piece images...');
                     $bar = $this->output->createProgressBar(PieceImage::all()->count());
                     $bar->start();
                     foreach (PieceImage::all() as $image) {
                         if (file_exists($image->imagePath.'/'.$image->fullsizeFileName)) {
-                            $file = Image::make($image->imagePath.'/'.$image->fullsizeFileName);
-                            if ($file->mime() != config('aldebaran.settings.image_formats.full')) {
+                            $fullFile = Image::make($image->imagePath.'/'.$image->fullsizeFileName);
+                            if ($fullFile->mime() != config('aldebaran.settings.image_formats.full')) {
                                 unlink($image->imagePath.'/'.$image->fullsizeFileName);
                             } else {
-                                unset($file);
-                            }
-
-                            // Set the display extension to ensure the display image/thumbnail
-                            // remain accessible even if display images are not likewise updated
-                            $image->display_extension = $image->extension;
-                            $image->extension = config('aldebaran.settings.image_formats.full');
-                            $image->save();
-
-                            if (isset($file)) {
-                                $file->save($image->imagePath.'/'.$image->fullsizeFileName, null, config('aldebaran.settings.image_formats.full'));
-                                unset($file);
+                                unset($fullFile);
                             }
                         }
-                        $bar->advance();
-                    }
-                    $bar->finish();
-                    $this->line("\n");
-                } elseif ((config('aldebaran.settings.image_formats.display') || config('aldebaran.settings.image_formats.full')) && $this->confirm('Do you want to update piece display and thumbnail images to '.(config('aldebaran.settings.image_formats.display') ?? config('aldebaran.settings.image_formats.full')).' now?'.(config('aldebaran.settings.image_formats.admin_view') ? ' Note that they will appear as '.config('aldebaran.settings.image_formats.admin_view').' files in the admin panel for convenience.' : ''))) {
-                    $format = config('aldebaran.settings.image_formats.display') ?? config('aldebaran.settings.image_formats.full');
-                    $bar = $this->output->createProgressBar(PieceImage::all()->count());
-                    $bar->start();
-                    foreach (PieceImage::all() as $image) {
                         if (file_exists($image->imagePath.'/'.$image->imageFileName)) {
                             $displayFile = Image::make($image->imagePath.'/'.$image->imageFileName);
-                            if ($displayFile->mime() != $format) {
+                            if ($displayFile->mime() != config('aldebaran.settings.image_formats.full')) {
                                 unlink($image->imagePath.'/'.$image->imageFileName);
                             } else {
                                 unset($displayFile);
@@ -154,36 +98,110 @@ class UpdateImages extends Command {
                         }
                         if (file_exists($image->imagePath.'/'.$image->thumbnailFileName)) {
                             $thumbFile = Image::make($image->imagePath.'/'.$image->thumbnailFileName);
-                            if ($thumbFile->mime() != $format) {
+                            if ($thumbFile->mime() != config('aldebaran.settings.image_formats.full')) {
                                 unlink($image->imagePath.'/'.$image->thumbnailFileName);
                             } else {
                                 unset($thumbFile);
                             }
                         }
 
-                        if (config('aldebaran.settings.image_formats.display') && (config('aldebaran.settings.image_formats.display') != config('aldebaran.settings.image_formats.full'))) {
-                            // Supply the display extension, but only if necessary
-                            $image->display_extension = config('aldebaran.settings.image_formats.display');
-                        } elseif (isset($image->display_extension)) {
-                            // Otherwise, if it was already set (e.g. above), make it null
-                            $image->display_extension = null;
-                        }
-                        $image->save();
+                        $image->update([
+                            'extension'         => config('aldebaran.settings.image_formats.full'),
+                            'display_extension' => null,
+                        ]);
 
+                        if (isset($fullFile)) {
+                            $fullFile->save($image->imagePath.'/'.$image->fullsizeFileName, null, config('aldebaran.settings.image_formats.full'));
+                            unset($fullFile);
+                        }
                         if (isset($displayFile)) {
-                            $displayFile->save($image->imagePath.'/'.$image->imageFileName, null, $format);
+                            $displayFile->save($image->imagePath.'/'.$image->imageFileName, null, config('aldebaran.settings.image_formats.full'));
                             unset($displayFile);
                         }
                         if (isset($thumbFile)) {
-                            $thumbFile->save($image->imagePath.'/'.$image->thumbnailFileName, null, $format);
+                            $thumbFile->save($image->imagePath.'/'.$image->thumbnailFileName, null, config('aldebaran.settings.image_formats.full'));
                             unset($thumbFile);
                         }
+
                         $bar->advance();
                     }
                     $bar->finish();
                     $this->line("\n");
                 } else {
-                    $this->line('Skipped updating piece images.');
+                    if (config('aldebaran.settings.image_formats.full') && $this->confirm('Do you want to update piece full-size images to '.config('aldebaran.settings.image_formats.full').' now?'.(config('aldebaran.settings.image_formats.admin_view') ? ' Note that they will appear as '.config('aldebaran.settings.image_formats.admin_view').' files in the admin panel for convenience.' : ''))) {
+                        $this->line('Updating piece full-size images...');
+                        $bar = $this->output->createProgressBar(PieceImage::all()->count());
+                        $bar->start();
+                        foreach (PieceImage::all() as $image) {
+                            if (file_exists($image->imagePath.'/'.$image->fullsizeFileName)) {
+                                $file = Image::make($image->imagePath.'/'.$image->fullsizeFileName);
+                                if ($file->mime() != config('aldebaran.settings.image_formats.full')) {
+                                    unlink($image->imagePath.'/'.$image->fullsizeFileName);
+                                } else {
+                                    unset($file);
+                                }
+
+                                // Set the display extension to ensure the display image/thumbnail
+                                // remain accessible even if display images are not likewise updated
+                                $image->display_extension = $image->extension;
+                                $image->extension = config('aldebaran.settings.image_formats.full');
+                                $image->save();
+
+                                if (isset($file)) {
+                                    $file->save($image->imagePath.'/'.$image->fullsizeFileName, null, config('aldebaran.settings.image_formats.full'));
+                                    unset($file);
+                                }
+                            }
+                            $bar->advance();
+                        }
+                        $bar->finish();
+                        $this->line("\n");
+                    } elseif ((config('aldebaran.settings.image_formats.display') || config('aldebaran.settings.image_formats.full')) && $this->confirm('Do you want to update piece display and thumbnail images to '.(config('aldebaran.settings.image_formats.display') ?? config('aldebaran.settings.image_formats.full')).' now?'.(config('aldebaran.settings.image_formats.admin_view') ? ' Note that they will appear as '.config('aldebaran.settings.image_formats.admin_view').' files in the admin panel for convenience.' : ''))) {
+                        $format = config('aldebaran.settings.image_formats.display') ?? config('aldebaran.settings.image_formats.full');
+                        $bar = $this->output->createProgressBar(PieceImage::all()->count());
+                        $bar->start();
+                        foreach (PieceImage::all() as $image) {
+                            if (file_exists($image->imagePath.'/'.$image->imageFileName)) {
+                                $displayFile = Image::make($image->imagePath.'/'.$image->imageFileName);
+                                if ($displayFile->mime() != $format) {
+                                    unlink($image->imagePath.'/'.$image->imageFileName);
+                                } else {
+                                    unset($displayFile);
+                                }
+                            }
+                            if (file_exists($image->imagePath.'/'.$image->thumbnailFileName)) {
+                                $thumbFile = Image::make($image->imagePath.'/'.$image->thumbnailFileName);
+                                if ($thumbFile->mime() != $format) {
+                                    unlink($image->imagePath.'/'.$image->thumbnailFileName);
+                                } else {
+                                    unset($thumbFile);
+                                }
+                            }
+
+                            if (config('aldebaran.settings.image_formats.display') && (config('aldebaran.settings.image_formats.display') != config('aldebaran.settings.image_formats.full'))) {
+                                // Supply the display extension, but only if necessary
+                                $image->display_extension = config('aldebaran.settings.image_formats.display');
+                            } elseif (isset($image->display_extension)) {
+                                // Otherwise, if it was already set (e.g. above), make it null
+                                $image->display_extension = null;
+                            }
+                            $image->save();
+
+                            if (isset($displayFile)) {
+                                $displayFile->save($image->imagePath.'/'.$image->imageFileName, null, $format);
+                                unset($displayFile);
+                            }
+                            if (isset($thumbFile)) {
+                                $thumbFile->save($image->imagePath.'/'.$image->thumbnailFileName, null, $format);
+                                unset($thumbFile);
+                            }
+                            $bar->advance();
+                        }
+                        $bar->finish();
+                        $this->line("\n");
+                    } else {
+                        $this->line('Skipped updating piece images.');
+                    }
                 }
             }
         }
