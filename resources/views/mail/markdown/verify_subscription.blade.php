d014bd34a598636f3a127f4348b0608c294fdead
itinerare <ad@itinerare.net>
2023-02-24 10:34:37 -0500

    fix(mailingLists): un-indent markdown mail - ignore that directory when formatting blade files

--- resources/views/mail/markdown/verify_subscription.blade.php
+++ resources/views/mail/markdown/verify_subscription.blade.php
@@ -1,13 +1,13 @@
 <x-mail::message>
 
-    You are receiving this email because you requested a subscription to the {{ $subscriber->mailingList->name }} mailing list. If you did not request this subscription, feel free to disregard this email.
+You are receiving this email because you requested a subscription to the {{ $subscriber->mailingList->name }} mailing list. If you did not request this subscription, feel free to disregard this email.
 
-    <x-mail::button :url="$subscriber->verifyUrl" color="success">
-        Verify Address
-    </x-mail::button>
+<x-mail::button :url="$subscriber->verifyUrl" color="success">
+    Verify Address
+</x-mail::button>
 
-    <hr /><br />
+<hr /><br />
 
-    If you're having trouble clicking the "Verify Address button", copy and paste the following URL into your web browser: <a href="{{ $subscriber->verifyUrl }}">{{ $subscriber->verifyUrl }}</a>
+If you're having trouble clicking the "Verify Address button", copy and paste the following URL into your web browser: <a href="{{ $subscriber->verifyUrl }}">{{ $subscriber->verifyUrl }}</a>
 
 </x-mail::message>
