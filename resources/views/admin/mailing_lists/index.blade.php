212c1aeac3e46e62720fd2e5bc28f51aca386a88
itinerare <ad@itinerare.net>
2023-02-24 09:00:36 -0500

    feat(mailingLists): add unverified subscriber count to admin index

--- resources/views/admin/mailing_lists/index.blade.php
+++ resources/views/admin/mailing_lists/index.blade.php
@@ -27,7 +27,9 @@
                 <div class="d-flex row flex-wrap col-12 mt-1 pt-2 px-0 ubt-top">
                     <div class="col-12 col-md-2">{!! $list->is_open ? '<i class="text-success fas fa-check"></i>' : '' !!}</div>
                     <div class="col-12 col-md-4">{{ $list->name }}</div>
-                    <div class="col-6 col-md-2">{{ $list->subscribers->count() }}</div>
+                    <div class="col-6 col-md-2">
+                        {{ $list->subscribers()->verified()->count() }}
+                        {!! $list->subscribers()->verified(0)->count() ? ' <span class="text-muted">('.$list->subscribers()->verified(0)->count().' Unverified)</span>' : '' !!}</div>
                     <div class="col-6 col-md-3">{!! $list->entries->count() ? $list->entries->first()->subject . ' - ' . pretty_date($list->entries->first()->created_at) : 'None!' !!}</div>
                     <div class="col-3 col-md-1 text-right"><a href="{{ url('admin/mailing-lists/edit/' . $list->id) }}" class="btn btn-primary py-0 px-2">Edit</a></div>
                 </div>
