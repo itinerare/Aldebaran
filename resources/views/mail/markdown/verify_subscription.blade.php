69c76b31083434443fd84dfd23b1bef5365d057f
itinerare <ad@itinerare.net>
2023-02-28 11:59:29 -0500

    fix(mailingLists): move misplaced quotes in verification email template

--- resources/views/mail/markdown/verify_subscription.blade.php
+++ resources/views/mail/markdown/verify_subscription.blade.php
@@ -8,6 +8,6 @@ You are receiving this email because you requested a subscription to the {{ $sub
 
 <hr /><br />
 
-If you're having trouble clicking the "Verify Address button", copy and paste the following URL into your web browser: <a href="{{ $subscriber->verifyUrl }}">{{ $subscriber->verifyUrl }}</a>
+If you're having trouble clicking the "Verify Address" button, copy and paste the following URL into your web browser: <a href="{{ $subscriber->verifyUrl }}">{{ $subscriber->verifyUrl }}</a>
 
 </x-mail::message>
