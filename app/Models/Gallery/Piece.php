7c475e17e6d68b181fa92dfc81c9499f0352aace
itinerare <ad@itinerare.net>
2023-03-02 10:04:27 -0500

    fix(pieces): if main gallery is disabled, never consider pieces able to be shown in it

--- app/Models/Gallery/Piece.php
+++ app/Models/Gallery/Piece.php
@@ -268,6 +268,11 @@ class Piece extends Model implements Feedable {
      * @return bool
      */
     public function getShowInGalleryAttribute() {
+        // Check if the gallery is enabled to be displayed in
+        if (!config('aldebaran.settings.navigation.gallery')) {
+            return 0;
+        }
+
         // Check if the piece should be included in the gallery or not
         if ($this->whereRelation('tags.tag', 'is_active', false)->count()) {
             return 0;
