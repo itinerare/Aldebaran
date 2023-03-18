b4302b14e1eb4ca2c67acbfa7ef6540638a7950f
itinerare <ad@itinerare.net>
2023-03-18 11:54:05 -0400

    feat(commissions): make invoice fields on individual commissions context-sensitive - shows only fields relevant to the current payment processor - restructure invoice fields to be clearer regarding what is for which processor

--- resources/views/admin/commissions/_invoice_fields.blade.php
+++ resources/views/admin/commissions/_invoice_fields.blade.php
@@ -2,19 +2,28 @@
     {!! Form::label('product_name', 'Product Name' . (!isset($requireName) || !$requireName ? ' (Optional)' : '')) !!} {!! add_help('A succinct name for the good and/or service you are providing, e.g. \'Commissioned Art\'.') !!}
     {!! Form::text('product_name', $object->invoice_data ? $object->invoice_data['product_name'] : null, ['class' => 'form-control', 'placeholder' => isset($parent) && $parent ? $object->parentInvoiceData['product_name'] ?? null : '']) !!}
 </div>
-@if (config('aldebaran.commissions.payment_processors.paypal.integration.enabled'))
+@if ($object->id && isset($parent) && $parent)
+    <div class="form-group">
+        {!! Form::checkbox('unset_product_info', 1, 0, [
+            'class' => 'form-check-input',
+            'data-toggle' => 'toggle',
+            'data-onstyle' => 'danger',
+        ]) !!}
+        {!! Form::label('unset_product_info', 'Unset Product Information', ['class' => 'form-check-label ml-3']) !!} {!! add_help('Removes the product information set here.') !!}
+    </div>
+@endif
+@if (config('aldebaran.commissions.payment_processors.paypal.integration.enabled') && (isset($object->payment_processor) ? $object->payment_processor == 'paypal' : true))
+    <h4>PayPal Information</h4>
     <div class="form-group">
         {!! Form::label('product_description', 'Product Description (Optional)') !!}
-        @if (config('aldebaran.commissions.payment_processors.stripe.integration.enabled'))
-            {!! add_help('Note that this is not shown on Stripe invoices.') !!}
-        @endif
         {!! Form::text('product_description', $object->invoice_data ? $object->invoice_data['product_description'] : null, [
             'class' => 'form-control',
             'placeholder' => isset($parent) && $parent ? $object->parentInvoiceData['product_description'] ?? null : '',
         ]) !!}
     </div>
 @endif
-@if (config('aldebaran.commissions.payment_processors.stripe.integration.enabled'))
+@if (config('aldebaran.commissions.payment_processors.stripe.integration.enabled') && (isset($object->payment_processor) ? $object->payment_processor == 'stripe' : true))
+    <h4>Stripe Information</h4>
     <div class="form-group">
         {!! Form::label('product_tax_code', 'Tax Category Code (Optional)') !!} {!! add_help('The tax code for the relevant category. If unset, this defaults to your Stripe account\'s default tax category.') !!}
         {!! Form::text('product_tax_code', $object->invoice_data ? $object->invoice_data['product_tax_code'] : null, [
@@ -27,15 +36,5 @@
             @endif
             <a href="https://stripe.com/docs/tax/tax-categories">Tax Category Reference</a>
         </small>
-    </div>
-@endif
-@if ($object->id && isset($parent) && $parent)
-    <div class="form-group">
-        {!! Form::checkbox('unset_product_info', 1, 0, [
-            'class' => 'form-check-input',
-            'data-toggle' => 'toggle',
-            'data-onstyle' => 'danger',
-        ]) !!}
-        {!! Form::label('unset_product_info', 'Unset Product Information', ['class' => 'form-check-label ml-3']) !!} {!! add_help('Removes the product information set here.') !!}
     </div>
 @endif
