bb7239fe77859c5086cbe101b2b7ce4a52d62196
itinerare <ad@itinerare.net>
2023-04-06 14:57:17 -0400

    fix(commissions): add line break to admin index after each comm class' queues

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -65,6 +65,7 @@
                             </div>
                         </div>
                     </div>
+                    <div class="w-100"></div>
                 @endforeach
             </div>
         @else
